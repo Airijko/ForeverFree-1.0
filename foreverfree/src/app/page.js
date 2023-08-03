cd6378e480dcfe2ec4c27130d71856306d2c2beb
Airijko <airijkoblade@gmail.com>
2023-08-03 01:33:47 -0500

    Base App Created

---
+++ foreverfree/src/app/page.js
@@ -0,0 +1,16 @@
+import {
+  Box,
+  Button,
+  Stepper,
+  Step,
+  StepButton,
+  StepLabel,
+} from "@mui/material";
+
+export default function Home() {
+  return (
+    <main>
+      <Button>test</Button>
+    </main>
+  );
+}
