28a9e243e18239a00a1cba23301aece67ca94a3b
Airijko <airijkoblade@gmail.com>
2023-08-07 02:57:22 -0500

    refactor: removed mui components

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,6 @@
-import ChecklistIcon from '@mui/icons-material/Checklist';
-
 export default function Home() {
   return (
     <div>
-      <ChecklistIcon />
-
       <h1>Hello World!</h1>
     </div>
   );
